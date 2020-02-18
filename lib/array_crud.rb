50ececb99c3692597260e4afcdf43e64bd7d5b21
kyle molidor <k_molidor@yahoo.com>
2020-02-18 18:22:25 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,16 +8,16 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  animals.push["cow"]
+  array.push(element)
   
 end
 
 def add_element_to_start_of_array(array, element)
-  animals.unshift ["horse"]
+  
 end
 
 def remove_element_from_end_of_array(array)
-  animals.pop ["cow"]
+
 end
 
 def remove_element_from_start_of_array(array)
